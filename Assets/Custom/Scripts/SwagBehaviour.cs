6625ef305722ae4fd7155d8edd5e8c243d2f06fe
Kyle Zhou <kylezhou2002@hotmail.com>
2015-08-21 22:44:29 -0700

    added bunch of sounds and worked on main screen

---
+++ Assets/Custom/Scripts/SwagBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SwagBehaviour : MonoBehaviour
+{
+    public GameObject textBox;
+
+    private bool activated = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        textBox = GameObject.Find("Text");
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D yay)
+    {
+        if (activated || !yay.gameObject.tag.Equals("Player")) return;
+        textBox.GetComponent<Text>().text = "SWAGBLAZE";
+
+        activated = true;
+    }
+}
+
