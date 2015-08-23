96d10beb48cb8a394536a647857865cde2de2245
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-08-22 18:59:54 -0700

    Started working on picking up items

--- Assets/Custom/Scripts/GlobalState.cs
+++ Assets/Custom/Scripts/GlobalState.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GlobalState : Singleton<GlobalState> {
     public bool PlayerCanMove = true;
+	public List<string> inventory = new List<string>();
+
 }
