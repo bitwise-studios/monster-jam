b9c0697389987864a7190e552a35b9fc6a7da551
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-08-22 21:08:29 -0700

    Newspaper ending: started work on newspaper scene

---
+++ Assets/Custom/Scripts/NewspaperSetHeadlineBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NewspaperSetHeadlineBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text>().text = GlobalState.Instance.newspaperHeadline;
+	}
+
+}
